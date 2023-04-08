5fbc916483b477361d7e859b76988bd9623eda79
Enigma <sabuhiakbarli@gmail.com>
2023-04-08 14:09:11 +0400

    Maintenance...

---
+++ src/main/java/com.akbarlee.SpringServer/Token/VerificationTokenRepository.java
@@ -0,0 +1,9 @@
+package com.akbarlee.SpringServer.Token;
+
+import com.akbarlee.SpringServer.User.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken , Long> {
+     VerificationToken findByVerificationToken(String token);
+     VerificationToken findByUser(User user);
+}
