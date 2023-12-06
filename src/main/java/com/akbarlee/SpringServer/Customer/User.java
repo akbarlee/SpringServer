84fa5cad896625e0f52f8c9100f22730d43118da
Enigma <sabuhiakbarli@gmail.com>
2023-12-06 21:47:29 +0400

    Update role

--- src/main/java/com/akbarlee/SpringServer/Customer/User.java
+++ src/main/java/com/akbarlee/SpringServer/Customer/User.java
@@ -34,6 +34,7 @@ public class User implements UserDetails {
     private String lastname;
     @Column(unique=true)
     private String email;
+
     private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
@@ -61,6 +62,25 @@ public class User implements UserDetails {
     // @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     @Override
     public String getPassword() {
@@ -88,6 +108,7 @@ public class User implements UserDetails {
         return true;
     }
 
+    @Override
     public boolean isEnabled() {
         return isEnabled;
     }
